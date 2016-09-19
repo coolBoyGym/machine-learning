1e468d2fd0f38e8770454174bc3c79223bbb437e
Yimei <gaoyimei2015@163.com>
2016-09-19 14:15:35 +0800

    spam test use naive bayes.

--- naive_bayesian/bayes.py
+++ naive_bayesian/bayes.py
@@ -20,7 +20,7 @@ def createVocabList(dataSet):
         vocabSet = vocabSet | set(document)  # union of the two sets
     return list(vocabSet)
 
-# 将一段文本转化为词条向量
+# 将一段文本转化为词条向量(0-1类型)
 def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0] * len(vocabList)
     for word in inputSet:
@@ -28,6 +28,14 @@ def setOfWords2Vec(vocabList, inputSet):
             returnVec[vocabList.index(word)] = 1
         else:
             print "the word: %s is not in my Vocabulary!" % word
+    return returnVec
+
+# 将一段文本转化为词条向量(数值类型)
+def bagOfWords2VecMN(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
     return returnVec
 
 
@@ -49,6 +57,7 @@ def trainNB0(trainMatrix, trainCategory):
     p0Vect = log(p0Num/p0Denom)          # change to log()
     return p0Vect, p1Vect, pAbusive
 
+
 # 使用分类器对样本进行分类
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
     p1 = sum(vec2Classify * p1Vec) + log(pClass1)
@@ -65,13 +74,55 @@ def testingNB():
     trainMat = []
     for postinDoc in listOPosts:
         trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
-    p0V, p1V, pAb = trainNB0(array(trainMat),array(listClasses))
+    p0V, p1V, pAb = trainNB0(array(trainMat), array(listClasses))
+
     testEntry = ['love', 'my', 'dalmation']
     thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
     print testEntry, 'classified as:', classifyNB(thisDoc, p0V, p1V, pAb)
     testEntry = ['stupid', 'garbage']
     thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
     print testEntry, 'classified as:', classifyNB(thisDoc, p0V, p1V, pAb)
+
+# 将一段字符串转化为小写字符列表
+def textParse(bigString):    # input is big string, output is word list
+    import re
+    listOfTokens = re.split(r'\W*', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+
+# 自包含的垃圾邮件分类测试函数
+def spamTest():
+    docList = []
+    classList = []
+    fullText = []
+    for i in range(1, 26):  # 正例和负例分别有25个
+        wordList = textParse(open('email/spam/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)  # create vocabulary
+    trainingSet = range(50); testSet = []   # create train set and test set
+    for i in range(10):  # 取50个样本中的10个作为测试样本 剩下的作为训练样本
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+
+    trainMat = []; trainClasses = []
+    for docIndex in trainingSet:  # train the classifier (get probs) trainNB0
+        trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:      # classify the remaining items
+        wordVector = bagOfWords2VecMN(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+            print "classification error", docList[docIndex]
+    print 'the error rate is:', float(errorCount)/len(testSet)
 
 
 if __name__ == '__main__':
@@ -87,4 +138,6 @@ if __name__ == '__main__':
     # print 'p0V = ', p0V
     # print 'p1V = ', p1V
     # print 'pAb = ', pAb
-    testingNB()
+    #
+    # testingNB()
+    spamTest()
